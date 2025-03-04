ee2950a9a1915c8cab9f5c4e3a84d1c421f84542
beansdrawer <beansdrawer@naver.com>
2025-03-04 14:20:14 +0900

    file upload on aws s3 with python

---
+++ app-for-access.py
@@ -0,0 +1,15 @@
+import boto3
+from botocore.client import Config
+from dotenv import dotenv_values
+
+config = dotenv_values(".env") 
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=config['AWS_ACCESS_KEY'],
+    aws_secret_access_key=config['AWS_SECRET_KEY'],
+    config=Config(signature_version='s3v4')
+)
+
+with open('./banana.jpg', 'rb') as data:
+  s3.upload_fileobj(data, 'coubot', "banana.jpg")
